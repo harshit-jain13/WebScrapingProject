eb1a6463d86d793ce70753b1fa00722725784dac
harshit-jain13 <61181118+harshit-jain13@users.noreply.github.com>
2021-02-04 12:33:41 +0530

    Delete SeleniumMainLibraries.py

--- SeleniumMainLibraries.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import csv
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
